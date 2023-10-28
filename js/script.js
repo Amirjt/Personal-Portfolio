ddbc42731a9bbdb81a52fafe65aa8e258440ccc2
amir <emirkh165@gmail.com>
2023-10-28 16:09:03 +0330

    header and main completed

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const $ = document;
+
+// Active class for nav links
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    links.forEach((link) => {
+      link.classList.remove('active');
+    });
+    e.target.classList.add('active');
+  });
+});
+
+// Responsive Btn 
+const btn = $.querySelector("#res-btn");
+const mobileLinks = $.querySelector("#mobile-links");
+const closeBtn = $.querySelector("#close-btn");
+
+btn.addEventListener("click", () => {
+  mobileLinks.classList.remove("hidden");
+  mobileLinks.classList.add("flex");
+  mobileLinks.classList.add("slide-in");
+});
+
+closeBtn.addEventListener("click", () => {
+  mobileLinks.classList.remove("slide-in");
+  mobileLinks.classList.add("slide-out");
+  setTimeout(() => {
+    mobileLinks.classList.add("hidden");
+    mobileLinks.classList.remove("flex");
+    mobileLinks.classList.remove("slide-out");
+  }, 500);
+});
+
+//Animation for main
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      const desc = document.querySelector('.desc');
+      const lottie = $.querySelector(".lottie");
+      desc.style.opacity = '1';
+      lottie.style.opacity = '1'
+      desc.style.transform = 'translateY(0)';
+      lottie.style.transform = 'translateY(0)'
+    }, 500);
+  });
+  
